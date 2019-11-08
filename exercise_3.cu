0eb198d71467e700170d5d152c21171e871a81a3
Vincent Paul Lohse <vplohse@kth.se>
2019-11-08 18:09:02 +0100

    Slimmed out code a bit.

--- exercise_3.cu
+++ exercise_3.cu
@@ -4,8 +4,8 @@
 using namespace std::chrono;
 using namespace std;
 #define TPB 256
-#define NUM_PARTICLES 11
-#define NUM_ITERATIONS 100
+#define NUM_PARTICLES 10000
+#define NUM_ITERATIONS 1000
 #define N (NUM_PARTICLES/TPB + 1)
 
 
@@ -34,8 +34,8 @@ __host__ __device__ float gen_random(int seed, int particle_id, int iteration)
  __global__ void timestep(particle *particles, seed seed, int iteration) {
    const int i = blockIdx.x*blockDim.x + threadIdx.x;
    if (i<NUM_PARTICLES) {
-     printf("Old threadId = %d, velocity.x = %e, position.x = %e\n",
-        threadIdx.x, particles[i].velocity[0], particles[i].position[0]);
+      // printf("Old threadId = %d, velocity.x = %e, position.x = %e\n",
+      //    threadIdx.x, particles[i].velocity[0], particles[i].position[0]);
 
      // Velocity update:
       particles[i].velocity[0] = gen_random(seed.x, i, iteration);
@@ -53,36 +53,29 @@ __host__ __device__ float gen_random(int seed, int particle_id, int iteration)
 
 int main()
 {
-  // float *d_x;
-  // particle *d_ps
   seed seed = {5,6,7};
   auto start = high_resolution_clock::now();
   particle *particlesCPU = new particle[NUM_PARTICLES];
   particle *particlesGPU = new particle[NUM_PARTICLES];
 
   cudaMalloc(&particlesGPU, NUM_PARTICLES*6*sizeof(float));
-  // cudaMalloc(&particlesGPU, NUM_PARTICLES*sizeof(particlesGPU));
-
-  // cudaMalloc(&d_x, NUM_PARTICLES*6*sizeof(float));  // Allocate device data
-  // cudaMalloc(&d_p, sizeof(d_ps));  // Allocate helper struct on the device
-  // cudaMalloc(&particles, NUM_PARTICLES*sizeof(float));
-  // cudaMemcpy(d_x, x, NUM_PARTICLES*sizeof(float), cudaMemcpyHostToDevice);
 
   for (int i = 0; i < NUM_ITERATIONS; i++) {
-    cout << "iteration: " << i <<"\n";
+    // cout << "iteration: " << i <<"\n";
     timestep<<<N, TPB>>>(particlesGPU, seed, i);
-    cudaDeviceSynchronize();
+    // cudaDeviceSynchronize();
   }
 
   cudaMemcpy(particlesCPU, particlesGPU, NUM_PARTICLES*6*sizeof(float), cudaMemcpyDeviceToHost);
 
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<microseconds>(stop - start);
-  cout << "Duration in microseconds: " << duration.count() << "ms"<< endl;
+  cout << "Duration in microseconds: " << duration.count() << endl;
 
-  for (int ii = 0; ii < NUM_PARTICLES; ii++) {
-    cout << particlesCPU[ii].position[0] << "\n";
-  }
+  // Print output:
+  // for (int ii = 0; ii < NUM_PARTICLES; ii++) {
+  //   cout << particlesCPU[ii].position[0] << "\n";
+  // }
 
   delete[] particlesCPU;
   cudaFree(particlesGPU);
