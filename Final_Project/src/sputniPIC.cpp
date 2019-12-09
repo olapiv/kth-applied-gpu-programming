859952dfd3b9ccfe11d7f2427c820c4c68cd3881
Vincent Lohse <vincent.lohse@live.de>
2019-12-09 12:49:12 +0100

    Commented out CPU code in sputniPIC.cpp

--- Final_Project/src/sputniPIC.cpp
+++ Final_Project/src/sputniPIC.cpp
@@ -114,15 +114,15 @@ int main(int argc, char **argv){
     
         // Set to zero the densities - needed for interpolation
         // Only idn and ids are changed
-        setZeroDensities(&idn, ids, &grd, param.ns);
+        // setZeroDensities(&idn, ids, &grd, param.ns);
         setZeroDensities(&idnGPU2CPU, &idsGPU2CPU, &grd, param.ns);  // New for GPU
 
         cudaMemcpy(idsGPU, idsGPU2CPU, sizeof(interpDensSpecies) * param.ns, cudaMemcpyHostToDevice);
         
         // implicit mover
         iMover = cpuSecond(); // start timer for mover
-        for (int is=0; is < param.ns; is++)
-            mover_PC(&part[is],&field,&grd,&param);
+        // for (int is=0; is < param.ns; is++)
+        //    mover_PC(&part[is],&field,&grd,&param);
         eMover += (cpuSecond() - iMover); // stop timer for mover
         
         // Only particlesGPU is changed
@@ -131,8 +131,8 @@ int main(int argc, char **argv){
         // interpolation particle to grid
         iInterp = cpuSecond(); // start timer for the interpolation step
         // interpolate species
-        for (int is=0; is < param.ns; is++)
-            interpP2G(&part[is],&ids[is],&grd);
+        // for (int is=0; is < param.ns; is++)
+        //    interpP2G(&part[is],&ids[is],&grd);
 
         // Only ids is changed
         gpu_interpP2G<<<dim3(part->nop / TpBx + 1, 1, 1), dim3(TpBx, param.ns, 1)>>>(particlesGPU, idsGPU, grdGPU);
@@ -142,25 +142,25 @@ int main(int argc, char **argv){
         // apply BC to interpolated densities
         // Only ids is changed
         for (int is=0; is < param.ns; is++)
-            applyBCids(&ids[is],&grd,&param);
+            // applyBCids(&ids[is],&grd,&param);
             applyBCids(&idsGPU2CPU[is], &grd, &param);  // New for GPU
 
         // sum over species
         // Only idn is changed
-        sumOverSpecies(&idn, ids, &grd, param.ns);
+        // sumOverSpecies(&idn, ids, &grd, param.ns);
         sumOverSpecies(&idnGPU2CPU, idsGPU2CPU, &grd, param.ns);  // New for GPU
 
         // interpolate charge density from center to node
         // Only idn is changed 
         // TODO: Is idn changed though? Why not &idn.rhon?
-        applyBCscalarDensN(idn.rhon, &grd, &param);
+        // applyBCscalarDensN(idn.rhon, &grd, &param);
         applyBCscalarDensN(idnGPU2CPU.rhon, &grd, &param);  // New for GPU
         
         // write E, B, rho to disk
         if (cycle%param.FieldOutputCycle==0){
             // TODO: Comment this out
-            VTK_Write_Vectors(cycle, &grd, &field);
-            VTK_Write_Scalars(cycle, &grd, ids, &idn);
+            // VTK_Write_Vectors(cycle, &grd, &field);
+            // VTK_Write_Scalars(cycle, &grd, ids, &idn);
 
             VTK_Write_Vectors(cycle, &grd, &field);  // New for GPU
             VTK_Write_Scalars(cycle, &grd, idsGPU2CPU, &idnGPU2CPU);  // New for GPU
